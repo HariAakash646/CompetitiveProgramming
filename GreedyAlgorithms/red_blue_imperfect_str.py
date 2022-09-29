699c546c76053bb967e05d8e2e13856db8f786ac
HariAakash646 <71875794+HariAakash646@users.noreply.github.com>
2022-09-29 22:53:50 +0530

    Update red_blue_imperfect_str.py

--- GreedyAlgorithms/red_blue_imperfect_str.py
+++ GreedyAlgorithms/red_blue_imperfect_str.py
@@ -1,23 +1,30 @@
 t = int(input())
+
 for _ in range(t):
+    
     n = int(input())
     ltr_ls = list(input())
+    
     try:
         R_idx = ltr_ls.index("R")
     except ValueError:
         R_idx = 1000
+        
     try:
         B_idx = ltr_ls.index("B")
     except ValueError:
         B_idx = 1000
+        
     first = min(R_idx, B_idx)
     if first == 1000:
         first = 0
+        
     for i in range(first-1, -1, -1):
         if ltr_ls[i+1] == "B":
             ltr_ls[i] = "R"
         else:
             ltr_ls[i] = "B"
+            
     for i in range(first, n):
         if ltr_ls[i] != "?":
             continue
