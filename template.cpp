04f43fa5e425eb99d7e03752d3d50d008d8fec20
HariAakash646 <71875794+HariAakash646@users.noreply.github.com>
2022-12-19 16:33:12 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define all(cont) cont.begin(), cont.end()
+#define MP make_pair
+#define PB push_back
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
