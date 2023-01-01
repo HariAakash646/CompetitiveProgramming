1b9087b9910356d6f78589aca574cadecfbc81e1
HariAakash646 <71875794+HariAakash646@users.noreply.github.com>
2023-01-02 00:43:34 +0530

    Create couting_divisors_cses.cpp

---
+++ Math/couting_divisors_cses.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+#define scd(t) scanf("%d", &t)
+#define scld(t) scanf("%ld", &t)
+#define sclld(t) scanf("%lld", &t)
+#define scc(t) scanf("%c", &t)
+#define scs(t) scanf("%s", t)
+#define scf(t) scanf("%f", &t)
+#define sclf(t) scanf("%lf", &t)
+#define forr(i, j, k, in) for (int i = j; i < k; i += in)
+#define frange(i, j) forr(i, 0, j, 1)
+#define all(cont) cont.begin(), cont.end()
+#define MP make_pair
+#define PB push_back
+#define f first
+#define s second
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+typedef map<int, int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+typedef long int li;
+typedef unsigned long int uli;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+
+mpii prime_fac(int x) {
+    mpii out;
+    for(int i = 2; i * i <= x; i++) {
+        
+        if(x % i == 0) out[i] = 0;
+        while(x % i == 0) {
+            out[i]++;
+            x /= i;
+        }
+    }
+    if(x != 1) out[x] = 1;
+    return out;
+}
+
+int main()
+{
+    int n;
+    scd(n); 
+    int x;
+    frange(i, n) {
+        scd(x);
+        mpii fac = prime_fac(x);
+        int prod = 1;
+        for(pii p : fac) {
+            prod *= p.s + 1;
+        }
+        cout << prod << "\n";
+    }
+    
+    return 0;
+}
