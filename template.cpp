0f7ff54aeee4bc830effc8c3f04205f586ff22fa
Hari Aakash K <71875794+HariAakash646@users.noreply.github.com>
2023-02-05 10:27:49 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -10,20 +10,22 @@ using namespace std;
 #define scs(t) scanf("%s", t)
 #define scf(t) scanf("%f", &t)
 #define sclf(t) scanf("%lf", &t)
-#define forr(i, j, k, in) for (int i = j; i < k; i += in)
-#define frange(i, j) forr(i, 0, j, 1)
+#define forr(i, j, k) for (int i = j; i < k; i++)
+#define frange(i, j) forr(i, 0, j)
 #define all(cont) cont.begin(), cont.end()
-#define MP make_pair
-#define PB push_back
+#define pb push_back
+#define f first
+#define s second
+typedef long long int lli;
+typedef unsigned long long int ulli;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
+typedef vector<lli> vll;
 typedef vector<string> vs;
+typedef vector<bool> vb;
 typedef vector<pii> vii;
 typedef vector<vi> vvi;
+typedef vector<bool> vb;
 typedef map<int, int> mpii;
 typedef set<int> seti;
 typedef multiset<int> mseti;
-typedef long int li;
-typedef unsigned long int uli;
-typedef long long int lli;
-typedef unsigned long long int ulli;
