1b8d781d3ce9c8ca0f507db53b547eb6ac548c47
Hari Aakash <hariaakash646@gmail.com>
2024-04-19 11:11:15 +0530

    ordered set added

--- INOI_template/inoi_template.cpp
+++ INOI_template/inoi_template.cpp
@@ -540,3 +540,15 @@ void add(string str) {
 	}
 	cnt[x]++;
 }
+
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+
+typedef tree<
+	int,
+	null_type,
+	less<int>,
+	rb_tree_tag,
+	tree_order_statistics_node_update>
+	ordered_set;
